32f62380690aadf9bb3fffc04bae30009f3deded
Darren Victoriano <darren.victoriano@dexcom.com>
2024-01-31 15:29:27 -0800

    updated script to use main

--- reboot_jobs.py
+++ reboot_jobs.py
@@ -121,29 +121,25 @@ def reboot_namespace_list(NS_list, debug=True):
             
 
 ###################### UPDATE IMAGE METHODS ######################
-
-
-parser = argparse.ArgumentParser(formatter_class=RawTextHelpFormatter)
-parser.add_argument('pipeline', type=str, default="test", help='''"rt" - for built-in list of realtime job in patest namespace
-"hr" - for built-in list of hourly/bulkdata job in patest namespace
-"clinic" - for built-in list of clinic job in patest namespace
-"discovery" - for built-in list of uam-save jobs in patest namespace
-"spc" - for streaming-pipeline-creator job in patest namespace
-"all" - for built-in list of all pipline in patest namespace (except streaming-pipeline-creator)
-''')
-parser.add_argument("-d", '--debug', help='does not update; only prints out commands', action="store_true")
-parser.add_argument("-n", '--namespace', help='update specified namespace', action="store_true")
-parser.add_argument("-f", '--file', help='update from a text file containing a list of namespaces', action="store_true")
-args = parser.parse_args()
-
-if __name__ == "__main__":
+def main():
+    parser = argparse.ArgumentParser(formatter_class=RawTextHelpFormatter)
+    parser.add_argument('pipeline', type=str, default="test", help='''"rt" - for built-in list of realtime job in patest namespace
+    "hr" - for built-in list of hourly/bulkdata job in patest namespace
+    "clinic" - for built-in list of clinic job in patest namespace
+    "discovery" - for built-in list of uam-save jobs in patest namespace
+    "spc" - for streaming-pipeline-creator job in patest namespace
+    "all" - for built-in list of all pipline in patest namespace (except streaming-pipeline-creator)
+    ''')
+    parser.add_argument("-d", '--debug', help='does not update; only prints out commands', action="store_true")
+    parser.add_argument("-n", '--namespace', help='update specified namespace', action="store_true")
+    parser.add_argument("-f", '--file', help='update from a text file containing a list of namespaces', action="store_true")
+    args = parser.parse_args()
     pipeline = args.pipeline
     namespace_list = get_ns_list(pipeline)
     if args.namespace:
         namespace_list = [pipeline]
     if args.file:
         namespace_list = convert_txt_file_to_list(pipeline)
-    
     if args.debug:
         print("------- DEBUG RUN -------")
         print(f"Rebooting {len(namespace_list)} Job/s listed below:")
@@ -158,3 +154,7 @@ if __name__ == "__main__":
         reboot_namespace_list(NS_list=namespace_list, debug=False)
     exec_cmd('say -v Samantha "Reboot successfully completed."')
     print("------- ALL REBOOT DONE -------")
+
+
+if __name__ == "__main__":
+    main()
--- update_image.py
+++ update_image.py
@@ -353,25 +353,22 @@ def update_image_for_namespace_list(image, NS_list, debug=False, boot=False):
 
 
 ###################### UPDATE IMAGE METHODS ######################
-
-
-parser = argparse.ArgumentParser(formatter_class=RawTextHelpFormatter)
-parser.add_argument('image', type=str, help='''full image path example:
-    gcr.io/prod-us-5g-ops-1/spark-data-lake:master-gha-105''')
-parser.add_argument('pipeline', type=str, default="test", help='''"rt" - for built-in list of realtime job in patest namespace
-"hr" - for built-in list of hourly/bulkdata job in patest namespace
-"clinic" - for built-in list of clinic job in patest namespace
-"discovery" - for built-in list of uam-save jobs in patest namespace
-"spc" - for streaming-pipeline-creator job in patest namespace
-"all" - for built-in list of all pipline in patest namespace (except streaming-pipeline-creator)
-''')
-parser.add_argument("-d", '--debug', help='does not update; only prints out commands', action="store_true")
-parser.add_argument("-n", '--namespace', help='update specified namespace', action="store_true")
-parser.add_argument("-f", '--file', help='update from a text file containing a list of namespaces', action="store_true")
-parser.add_argument("-b", '--boot', help='check pods to boot before proceeding to update the next job.', action="store_true")
-args = parser.parse_args()
-
-if __name__ == "__main__":
+def main():
+    parser = argparse.ArgumentParser(formatter_class=RawTextHelpFormatter)
+    parser.add_argument('image', type=str, help='''full image path example:
+        gcr.io/prod-us-5g-ops-1/spark-data-lake:master-gha-105''')
+    parser.add_argument('pipeline', type=str, default="test", help='''"rt" - for built-in list of realtime job in patest namespace
+    "hr" - for built-in list of hourly/bulkdata job in patest namespace
+    "clinic" - for built-in list of clinic job in patest namespace
+    "discovery" - for built-in list of uam-save jobs in patest namespace
+    "spc" - for streaming-pipeline-creator job in patest namespace
+    "all" - for built-in list of all pipline in patest namespace (except streaming-pipeline-creator)
+    ''')
+    parser.add_argument("-d", '--debug', help='does not update; only prints out commands', action="store_true")
+    parser.add_argument("-n", '--namespace', help='update specified namespace', action="store_true")
+    parser.add_argument("-f", '--file', help='update from a text file containing a list of namespaces', action="store_true")
+    parser.add_argument("-b", '--boot', help='check pods to boot before proceeding to update the next job.', action="store_true")
+    args = parser.parse_args()
     pipeline = args.pipeline
     image = args.image
     namespace_list = get_ns_list(pipeline)
@@ -396,3 +393,7 @@ if __name__ == "__main__":
         update_image_for_namespace_list(image=image, NS_list=namespace_list, debug=False, boot=args.boot)
     exec_cmd('say -v Samantha "Image update successfully completed."')
     print("------- ALL UPDATES DONE -------")
+
+
+if __name__ == "__main__":
+    main()
